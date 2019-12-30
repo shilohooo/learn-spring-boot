abd2dad057eb33f1dc6f8982195eef13e15754f6
shilohooo <2544729685@qq.com>
2019-12-30 17:14:30 +0800

    1.注释位置调整

--- spring-security-start/src/main/java/org/shiloh/app/config/BrowserSecurityConfig.java
+++ spring-security-start/src/main/java/org/shiloh/app/config/BrowserSecurityConfig.java
@@ -65,45 +65,71 @@ public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // http.httpBasic() HTTP Basic方式
         http.exceptionHandling()
-                .accessDeniedHandler(accessDeniedHandler) // 指定没有权限访问时的处理器
+                // 指定没有权限访问时的处理器
+                .accessDeniedHandler(accessDeniedHandler)
                 .and()
-                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class) // 添加验证码校验过滤器，且在校验用户名和密码之前执行
-                .addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class) // 添加短信验证码校验过滤器
-                .formLogin()// 认证方式：基于表单的认证
-                .loginPage("/authentication/require") // 自定义登录页面，替换SpringSecurity的默认界面
-                .loginProcessingUrl("/login") // 登录请求url
-                .successHandler(authenticationSuccessHandler) // 登录成功的处理器
-                .failureHandler(authenticationFailureHandler) // 登录失败的处理器
+                // 添加验证码校验过滤器，且在校验用户名和密码之前执行
+                .addFilterBefore(validateCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                // 添加短信验证码校验过滤器
+                .addFilterBefore(smsCodeFilter, UsernamePasswordAuthenticationFilter.class)
+                // 认证方式：基于表单的认证
+                .formLogin()
+                // 自定义登录页面，替换SpringSecurity的默认界面
+                .loginPage("/authentication/require")
+                // 登录请求url
+                .loginProcessingUrl("/login")
+                // 登录成功的处理器
+                .successHandler(authenticationSuccessHandler)
+                // 登录失败的处理器
+                .failureHandler(authenticationFailureHandler)
                 .and()
-                .rememberMe() // 开启记住我功能
-                .tokenRepository(persistentTokenRepository()) // 指定token持久化仓库
-                .tokenValiditySeconds(3600) // 指定token有效时间（也就是记住我的失效时间），单位：秒
-                .userDetailsService(myUserDetailsService) // 自定义处理登录逻辑
+                // 开启记住我功能
+                .rememberMe()
+                // 指定token持久化仓库
+                .tokenRepository(persistentTokenRepository())
+                // 指定token有效时间（也就是记住我的失效时间），单位：秒
+                .tokenValiditySeconds(3600)
+                // 自定义处理登录逻辑
+                .userDetailsService(myUserDetailsService)
                 .and()
-                .authorizeRequests() // 授权配置
+                // 授权配置
+                .authorizeRequests()
+                // 无需认证的请求url
                 .antMatchers("/authentication/require",
                         "/login.html",
                         "/code/normal",
                         "/code/sms_code",
                         "/to/sms_login_page",
                         "/session_invalid",
-                        "/sign_out/success").permitAll() // 无需认证的请求url
-                .anyRequest().authenticated() // 除去antMatchers()方法配置的请求路径以外，所有请求url都需要认证
+                        "/sign_out/success").permitAll()
+                // 除去antMatchers()方法配置的请求路径以外，所有请求url都需要认证
+                .anyRequest().authenticated()
                 .and()
-                .sessionManagement() // 添加session管理器
-                .invalidSessionUrl("/session_invalid") // 指定session失效后需要跳转的url
-                .maximumSessions(1) // 配置session最大并发数量为1个，也就是同一个帐号在同一时刻只能登录1个
-                .maxSessionsPreventsLogin(true) // 当session达到最大有效数时，不再允许登录相同的用户
-                .expiredSessionStrategy(mySessionExpiredStrategy) // 指定session失效策略
+                // 添加session管理器
+                .sessionManagement()
+                // 指定session失效后需要跳转的url
+                .invalidSessionUrl("/session_invalid")
+                // 配置session最大并发数量为1个，也就是同一个帐号在同一时刻只能登录1个
+                .maximumSessions(1)
+                // 当session达到最大有效数时，不再允许登录相同的用户
+                .maxSessionsPreventsLogin(true)
+                // 指定session失效策略
+                .expiredSessionStrategy(mySessionExpiredStrategy)
                 .and()
                 .and()
                 .logout()
-                .logoutUrl("/sign_out") // 指定退出登录的url，默认为/logout
-                //.logoutSuccessUrl("/sign_out/success") // 退出登录成功后跳转的url，默认跳转到登录页面
-                .logoutSuccessHandler(logOutSuccessHandler) // 指定退出成功的处理器
-                .deleteCookies("JSESSIONID") // 退出后将key=JSESSIONID的cookie删除
-                .and().csrf().disable() // 关闭CSRF攻击防御
-                .apply(smsAuthenticationConfig); // 将短信验证码校验配置加入到SpringSecurity中
+                // 指定退出登录的url，默认为/logout
+                .logoutUrl("/sign_out")
+                // 退出登录成功后跳转的url，默认跳转到登录页面
+                //.logoutSuccessUrl("/sign_out/success")
+                // 指定退出成功的处理器
+                .logoutSuccessHandler(logOutSuccessHandler)
+                // 退出后将key=JSESSIONID的cookie删除
+                .deleteCookies("JSESSIONID")
+                // 关闭CSRF攻击防御
+                .and().csrf().disable()
+                // 将短信验证码校验配置加入到SpringSecurity中
+                .apply(smsAuthenticationConfig);
     }
 
     /**
