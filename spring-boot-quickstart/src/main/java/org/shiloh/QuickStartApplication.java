0e9733b156cc58c87150246e63f57b672904ded3
shilohooo <2544729685@qq.com>
2020-06-09 09:38:38 +0800

    1

--- spring-boot-quickstart/src/main/java/org/shiloh/QuickStartApplication.java
+++ spring-boot-quickstart/src/main/java/org/shiloh/QuickStartApplication.java
@@ -12,14 +12,15 @@ import static org.slf4j.LoggerFactory.getLogger;
 @SpringBootApplication
 public class QuickStartApplication {
 
-    public static final Logger logger = getLogger(QuickStartApplication.class);
+    private static final Logger LOGGER = getLogger(QuickStartApplication.class);
+
 
     public static void main(String[] args) {
-        logger.trace("***************log trace msg*****************");
-        logger.info("***************log info msg*****************");
-        logger.debug("***************log debug msg*****************");
-        logger.warn("***************log warning msg*****************");
-        logger.error("***************log error msg*****************");
+        LOGGER.trace("***************log trace msg*****************");
+        LOGGER.info("***************log info msg*****************");
+        LOGGER.debug("***************log debug msg*****************");
+        LOGGER.warn("***************log warning msg*****************");
+        LOGGER.error("***************log error msg*****************");
         SpringApplication.run(QuickStartApplication.class, args);
     }
 }
