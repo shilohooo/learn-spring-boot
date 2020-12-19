e558502c93ce525f9d9eb79614b11e6ea8e85ca4
shilohooo <lixiaolei595@gmail.com>
2020-12-19 08:58:04 +0800

    1.添加重定向参数接收测试

--- spring-boot-quickstart/src/main/java/org/shiloh/web/controller/HelloController.java
+++ spring-boot-quickstart/src/main/java/org/shiloh/web/controller/HelloController.java
@@ -5,7 +5,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @Author Shiloh
@@ -20,5 +24,15 @@ public class HelloController {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE);
         return new ResponseEntity<>("Hello World", httpHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping("/redirect-test")
+    public void redirectTest(HttpServletResponse response) throws IOException {
+        response.sendRedirect("http://localhost:8083/redirect-params?TokenAuthResponse=shiloh");
+    }
+
+    @GetMapping("/redirect-params")
+    public void getRedirectParams(@RequestParam("TokenAuthResponse") String tokenAuthResponse) {
+        System.out.println("tokenAuthResponse = " + tokenAuthResponse);
     }
 }
